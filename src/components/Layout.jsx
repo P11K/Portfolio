714af319190bddcc665e3cf8f86cf776f8976a2d
Vicko Putra <vickoardiansyah86@gmail.com>
2025-11-11 01:23:30 +0700

    Routebr

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
