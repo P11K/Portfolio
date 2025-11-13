3c79b7c387a9c6bff83dcc4db2c03ff5cd4fe46d
Vicko Putra <vickoardiansyah86@gmail.com>
2025-11-13 21:48:34 +0700

    Minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-// App.jsx
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import ViewCV from "./pages/view"; // ganti dari ViewPDF ke ViewCV
+import ViewPDF from "./pages/viewpdf"; // <--- ganti nama import
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="cv" element={<ViewCV />} />
+        <Route path="cv" element={<ViewPDF />} /> {/* <--- tambahkan route ini */}
       </Route>
     </Routes>
   );
